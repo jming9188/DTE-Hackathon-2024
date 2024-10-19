0113502aa0414c7d140545c6f28a9175549e5b42
Justin <roblujm@gmail.com>
2024-10-19 15:38:41 -0400

    result pie chart

--- frontend/src/Results.js
+++ frontend/src/Results.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import ApexChart from './Piechart';
 function Results() {
     return (
       <div>
+        <ApexChart/>
         <h1>Hello from MyComponent!</h1>
       </div>
     );
