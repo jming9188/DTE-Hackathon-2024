5f1588eba3c0ab852a565ce3c8a17da5a4bb4fee
Justin <roblujm@gmail.com>
2024-10-19 18:59:06 -0400

    fixed array sending to backend

--- predictions.py
+++ predictions.py
@@ -14,23 +14,26 @@ def predict():
         return '', 200
     
     try:
+        print("test1")
         data = request.get_json()
+        print("Data received:", data)
+        print("Keys in received data:", data.keys())
         age = data['age']
         education = data['education']
-        sex = data['sex']
-        is_smoking = data['is_smoking']
-        cigsPerDay = data.get('cigsPerDay', 0)  
-        BPMeds = data['BPMeds']
-        prevalentStroke = data['prevalentStroke']
-        prevalentHyp = data['prevalentHyp']
-        diabetes = data['diabetes']
+        sex = data['sex'] #
+        is_smoking = data['is_smoking'] #
+        cigsPerDay = data.get('cigsPerDay', 0) 
+        BPMeds = data['BPMeds'] #
+        prevalentStroke = data['prevalentStroke'] #
+        prevalentHyp = data['prevalentHyp'] #
+        diabetes = data['diabetes'] #
         totChol = data['totChol']
         sysBP = data['sysBP']
         diaBP = data['diaBP']
         BMI = data['BMI']
         heartRate = data['heartRate']
         glucose = data['glucose']
-
+        
         new_data = np.array([[age, education, sex, is_smoking, cigsPerDay, BPMeds,
                               prevalentStroke, prevalentHyp, diabetes, totChol,
                               sysBP, diaBP, BMI, heartRate, glucose]])
@@ -43,7 +46,9 @@ def predict():
             'confidence': float(prediction_proba[0][prediction[0]]) 
         })
     except Exception as e:
+        print("An error occurred:", str(e))
         return jsonify({'error': str(e)})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
