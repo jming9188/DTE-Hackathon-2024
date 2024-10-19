ae0e35c168e45a6a24ecf2b4ca07de4dbf4afa09
Justin <roblujm@gmail.com>
2024-10-19 12:29:09 -0400

    form start

---
+++ frontend/src/Form.js
@@ -0,0 +1,31 @@
+
+import { useCallback } from 'react';
+import 'survey-core/defaultV2.min.css';
+import { Model } from 'survey-core';
+import { Survey } from 'survey-react-ui';
+
+const surveyJson = {
+  elements: [{
+    name: "FirstName",
+    title: "Enter your first name:",
+    type: "text"
+  }, {
+    name: "LastName",
+    title: "Enter your last name:",
+    type: "text"
+  }]
+};
+
+function Form() {
+  const survey = new Model(surveyJson);
+  const alertResults = useCallback((sender) => {
+    const results = JSON.stringify(sender.data);
+    alert(results);
+  }, []);
+
+  survey.onComplete.add(alertResults);
+
+  return <Survey model={survey} />;
+}
+
+export default Form;
