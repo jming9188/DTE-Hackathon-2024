2be8a78b82bf520d7857c7a95e472b67a42428d9
StrBoll <phillipboll107@gmail.com>
2024-10-19 15:50:40 -0400

    CORS WONT WORK

--- predictions.py
+++ predictions.py
@@ -3,23 +3,19 @@ from flask_cors import CORS
 import numpy as np
 import joblib
 
-
 model = 'retrained_CHF_model.pkl'
 clf = joblib.load(model)
 app = Flask(__name__)
-CORS(app)
 
+# Configure CORS to allow requests from your frontend
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
 
-
-
-
-
-
-
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['OPTIONS', 'POST'])
 def predict():
+    if request.method == 'OPTIONS':
+        return '', 200
+    
     try:
-        
         data = request.get_json()
         age = data['age']
         education = data['education']
@@ -37,7 +33,6 @@ def predict():
         heartRate = data['heartRate']
         glucose = data['glucose']
 
-        
         new_data = np.array([[age, education, sex, is_smoking, cigsPerDay, BPMeds,
                               prevalentStroke, prevalentHyp, diabetes, totChol,
                               sysBP, diaBP, BMI, heartRate, glucose]])
@@ -49,9 +44,7 @@ def predict():
             'prediction': int(prediction[0]),  
             'confidence': float(prediction_proba[0][prediction[0]]) 
         })
-
     except Exception as e:
-        
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
