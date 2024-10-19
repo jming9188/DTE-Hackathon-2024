9a0e93724e49ab41fe6e9c51e194d772eee55812
Austin Nickse <goobersimon96@gmail.com>
2024-10-19 18:55:41 -0400

    Fixes formatting

--- frontend/src/Results.js
+++ frontend/src/Results.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ApexChart from './Piechart';
-import NextSteps from './NextSteps'
+import NextSteps from './NextSteps';
 function Results() {
     return (
       <div>
         <NextSteps/>
         <ApexChart/>
-        <h1>Hello from MyComponent!</h1>
       </div>
     );
   }
