435ff1cd7222365977b1839e3807ed4dcd11a32a
harshil-pahuja <harshilpahuja1987@gmail.com>
2024-10-20 00:17:09 +0000

    Made unordered list for displaying next steps

--- frontend/src/NextSteps.js
+++ frontend/src/NextSteps.js
@@ -7,23 +7,15 @@ const NextSteps = () => {
             <h1> Next Steps</h1>
             <div>
                 <p> Congestive Heart Failure (CHF) is a serious condition,
-                    but there are several proactive steps you can take to reduce your risk of developing it.
+                    but here are steps you should take based on your input data. 
                 </p>
-                <p>
-                    If you believe your risk level is high, it's wise to consult a healthcare professional. 
-                    You can either reach out to your current doctor or schedule a quick online appointment 
-                    through a service like the CVS Minute Clinic: <a href="https://www.cvs.com/minuteclinic/virtual-care" target="_blank" rel="noopener noreferrer">CVS Minute Clinic</a>
-                </p>
-                <p>
-                    Adopting healthier habits is another effective way to lower your risk of CHF.
-                    Maintaining a healthy weight, exercising regularly, eating a balanced diet, 
-                    quitting smoking, getting adequate sleep, and managing stress are all beneficial countermeasures.
-                </p>
-                <p>
-                    Your mental health is equally important. If you're feeling distressed about your health or need support
-                    in managing stress, consider reaching out to a mental health professional. 
-                    You can find a local psychiatrist through this link: <a href="https://www.psychologytoday.com/us/psychiatrists" target="_blank" rel="noopener noreferrer">Psychology Today</a>
-                </p>
+                <ul>
+                    <li>Schedule a physical with your current doctor or schedule an online 
+                    appointment through the CVS Minute Clinic: <a href="https://www.cvs.com/minuteclinic/virtual-care" target="_blank" rel="noopener noreferrer">CVS Minute Clinic</a></li>
+                    <li>Exercise for 30 minutes a day to lose weight and manage stress. </li> 
+                    <li>Increase your consumption of fruits and vegetables. </li>
+                    <li>Guidance from mental health professionals at the following link will give you additional support: <a href="https://www.psychologytoday.com/us/psychiatrists" target="_blank" rel="noopener noreferrer">Psychology Today</a></li>
+                </ul>
             </div>
         </div>
     );
