1b793e2c85ac9c46fd9b35279a2e4e4956019a37
Austin Nickse <goobersimon96@gmail.com>
2024-10-19 21:22:07 -0400

    Bullet point format

--- frontend/src/NextSteps.js
+++ frontend/src/NextSteps.js
@@ -10,11 +10,12 @@ const NextSteps = () => {
                     but here are steps you should take based on your input data. 
                 </p>
                 <ul>
-                    <li>Schedule a physical with your current doctor or schedule an online 
-                    appointment through the CVS Minute Clinic: <a href="https://www.cvs.com/minuteclinic/virtual-care" target="_blank" rel="noopener noreferrer">CVS Minute Clinic</a></li>
+                    <li>Schedule a physical with your current doctor</li>
+                    <li>Or schedule an online appointment through the CVS Minute Clinic:&nbsp;
+                    <a href="https://www.cvs.com/minuteclinic/virtual-care" target="_blank" rel="noopener noreferrer">CVS Minute Clinic</a></li>
                     <li>Exercise for 30 minutes a day to lose weight and manage stress. </li> 
                     <li>Increase your consumption of fruits and vegetables. </li>
-                    <li>Guidance from mental health professionals at the following link will give you additional support: <a href="https://www.psychologytoday.com/us/psychiatrists" target="_blank" rel="noopener noreferrer">Psychology Today</a></li>
+                    <li>Consider guidance from mental health professionals at:&nbsp;  <a href="https://www.psychologytoday.com/us/psychiatrists" target="_blank" rel="noopener noreferrer">Psychology Today</a></li>
                 </ul>
             </div>
         </div>
