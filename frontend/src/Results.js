76efc6b48ccc5c0a6d3611cb3842540e64167f5d
Justin <roblujm@gmail.com>
2024-10-19 20:50:05 -0400

    results colors

--- frontend/src/Results.js
+++ frontend/src/Results.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ApexChart from './Piechart';
 import NextSteps from './NextSteps';
 import { useLocation } from 'react-router-dom';
-
+import './result.css'
 function Results() {
   const location = useLocation();
   const { prediction, confidence } = location.state || {};
 
   return (
     <div className='result'>
-      <NextSteps/>
       <h1>{confidence*100}% chance you will have CHF withn 10 years </h1>
       <ApexChart value1={confidence*100} value2={100 - confidence*100}/>
+      <NextSteps/>
     </div>
   );
 }
